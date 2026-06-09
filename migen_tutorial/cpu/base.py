94d5f1df64dd45e729851277ab1649c91a7fa0ea
Carlos Camargo <cicamargoba@gmail.com>
2026-06-09 08:05:36 -0500

    Adding mult instance to Litex

--- migen_tutorial/cpu/base.py
+++ migen_tutorial/cpu/base.py
@@ -1,44 +1,47 @@
 #!/usr/bin/env python3
-# export LITEX_ENV_EFINITY=/home/carlos/Embedded/efinity/2023.2/
+import os
+import sys
+import env_efinity
 from migen import *
-from board import ecb_t8_t113
 from litex.build.generic_platform import *
-from litex.build.efinix import EfinixPlatform
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
 from litex.soc.cores import dna
 from litex.soc.cores.hyperbus import HyperRAM
 from liteeth.phy.rmii import LiteEthPHYRMII
+from pwm import PWM
+from platforms import get_platform, load_bitstream, parse_args, BUILD_DIR
 
-
-
-from pwm import PWM 
-platform = ecb_t8_t113.Platform()
+# Design
 class BaseSoC(SoCCore):
-    def __init__(self, platform, **kwargs):
-        sys_clk_freq = int(33.333e6)
+    def __init__(self, platform, sys_clk_freq, **kwargs):
         SoCCore.__init__(self, platform,
             cpu_type                 = "femtorv",
-            cpu_variant              = "gracilis",  #RV32IMCzCSR
-            clk_freq                 = 33.333e6,
+            cpu_variant              = "gracilis",
+            clk_freq                 = sys_clk_freq,
             ident                    = "LiteX CPU Test SoC", ident_version=True,
-            integrated_rom_size      = 0x8000
-            #integrated_main_ram_size = 0x4000
+            integrated_rom_size      = 0x8000,
         )
-
-
-
-        self.submodules.crg = CRG(platform.request("clk33"), ~platform.request("user_btn_n")) 
+        self.submodules.crg = CRG(platform.request("clk33"), ~platform.request("user_btn_n"))
         self.ethphy = LiteEthPHYRMII(
-          clock_pads = self.platform.request("eth_clocks"),
-          pads       = self.platform.request("eth"),
-          refclk_cd  = None)
+            clock_pads = self.platform.request("eth_clocks"),
+            pads       = self.platform.request("eth"),
+            refclk_cd  = None)
         self.add_ethernet(phy=self.ethphy, data_width=32)
-
-        self.submodules.pwm = PWM(platform.request("user_led",  0))   # PWM
+        self.submodules.pwm = PWM(platform.request("user_led_n", 0))
         self.add_csr("pwm")
-soc = BaseSoC(platform)
-# Build --------------------------------------------------------------------------------------------
-builder = Builder(soc, output_dir="build/", csr_csv="csr.csv")
-builder.build(build_name="top")
 
+TopModule    = BaseSoC  # <-- cambia aquí el módulo a sintetizar
+
+# Main
+if __name__ == "__main__":
+    args = parse_args()
+    os.makedirs(BUILD_DIR, exist_ok=True)
+
+    platform, sys_clk_freq, led_name, bitstream = get_platform(args.platform)
+    top = TopModule(platform, sys_clk_freq)
+
+    builder = Builder(top, output_dir=BUILD_DIR, csr_csv="csr.csv")
+    builder.build(build_name="top")
+
+    load_bitstream(platform, args.platform, BUILD_DIR, bitstream)
